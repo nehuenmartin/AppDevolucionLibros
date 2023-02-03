caba853caf3dc9bc4fea45fdf74cb15b04650408
Nehuen <122373723+nehuenmartin@users.noreply.github.com>
2023-02-03 16:36:51 -0300

    Update AppLibros.py

--- AppLibros.py
+++ AppLibros.py
@@ -1,9 +1,26 @@
 from datetime import date
 from datetime import datetime, timedelta
+import calendar
+
 def main():
-    fecha_entrega = datetime.now() + timedelta(days=7)
+    fecha_entrega = date.today() + timedelta(days=14)
+    day = fecha_entrega.day
+    yy = fecha_entrega.year
+    mm = fecha_entrega.month
+    cal = calendar.monthcalendar(yy, mm)
+   
     cantidad()
     print("Fecha de entrega: ", fecha_entrega)
+    for i in cal:
+        for j in i:
+            if j == 0:
+                print("   ", end="")
+            elif j == day:
+                print("[%2d]" % j, end="")
+            else:
+                print(" %2d " % j, end="")
+        print("")
+
 def cantidad():
     value = int(input("Cantidad de libros: "))
     books = []
@@ -24,14 +41,10 @@ def realizar(books):
 
 def imprimir(lista_books):
     for libro in lista_books:
-        print("---")
+        print("--- Libro a entregrar ---")
         print("Nombre:", libro["nombre"])
         print("Categoria:", libro["categoria"])
         print("Autor:", libro["autor"])
         print("Piso:", libro["piso"])
-        print("---")
+        print("------------------------")
 main()
-
-
-
-
